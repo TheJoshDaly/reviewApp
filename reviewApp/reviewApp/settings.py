6963ff62936ee1340f39531ff27dc567d1e3d1a7
TheJoshDaly <34450383+TheJoshDaly@users.noreply.github.com>
2020-04-27 20:37:20 +0100

    Add files via upload
    - Fixed signals
    - Fixed links
    - Fixed models
    - Altered settings and routes properly

--- reviewApp/reviewApp/settings.py
+++ reviewApp/reviewApp/settings.py
@@ -124,5 +124,5 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-LOGIN_REDIRECT_URL = 'reviewapp'
+LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
